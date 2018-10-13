5b15d3cb1c04fc0a3dca36972fed77875974da62
Henry <hanke.liu@gmail.com>
2018-10-13 00:05:08 -0400

    passed

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
 
+def my_collect(array)
+  collect = []
 
+  i = 0
+  while i < array.length
+    collect << yield(array[i])
+    i += 1
+  end
+  collect
+
+end
+
+
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#
+# #first_name_array = []
+#
+#
+#
+# my_collect(array) do |name|
+#   first_name_array.push(name.split(" ").first)
+# end
